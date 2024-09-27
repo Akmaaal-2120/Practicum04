c851c81b41cf1b62a0b7463f416409cb603a49b3
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-09-27 07:45:39 +0700

    tes

--- PemilihanBilangan15.java
+++ PemilihanBilangan15.java
@@ -3,7 +3,7 @@ public class PemilihanBilangan15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan sebuah angka : ");
+        System.out.print(" Masukkan sebuah angka : ");
         int angka = sc.nextInt();
 
         if(angka % 2 == 0){
