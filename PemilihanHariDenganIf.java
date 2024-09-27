8d4a3031461b02f01a0831282ec51783544cc65d
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-09-27 09:12:30 +0700

    Pemilihan hari dengan if

---
+++ PemilihanHariDenganIf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PemilihanHariDenganIf {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int numbers;
+
+        System.out.print("Enter your number : ");
+        numbers = sc.nextInt();
+
+        if(numbers > 1 && numbers <= 5){
+            System.out.println("Weekday");
+        }else if(numbers == 6 || numbers == 7){
+            System.out.println("Weekend");
+        }else{
+            System.out.println("Invalid number");
+        }
+
+    }
+}
