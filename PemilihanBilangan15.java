015b00d5c40619e6fc9d6a45f5a378fd0ae3b24a
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-09-27 07:54:52 +0700

    update ternary

--- PemilihanBilangan15.java
+++ PemilihanBilangan15.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 public class PemilihanBilangan15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String hasil;
         System.out.print(" Masukkan sebuah angka : ");
         int angka = sc.nextInt();
 
-        if(angka % 2 == 0){
-            System.out.println("Angka " + angka + " Termasuk bilangan genap ");
+        hasil = (angka % 2 == 0) ? " Termasuk bilangan genap " : " Termasuk bilangan ganji";
 
-        }else{
-            System.out.println("Angka " + angka + " Termasuk bilangan ganjil ");
-        }
+        System.out.println(angka + hasil);
 
 
     }
